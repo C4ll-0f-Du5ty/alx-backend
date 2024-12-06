5433c241598ab3afdeec1589bd450c7ca7ee1d27
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-12-06 11:20:02 +0200

    done

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ LRU Caching """
 
--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,13 +7,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
-@app.route("/", methods=["GET"], strict_slashes=False)
 
+@app.route("/", methods=["GET"], strict_slashes=False)
 def helloWorld() -> str:
     """
     Render template for Babel usage
     """
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run()
